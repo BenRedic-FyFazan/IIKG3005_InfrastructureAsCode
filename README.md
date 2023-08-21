b4412ef24c7480b059b324695a833ee3d8c9ebe0
Karl-Henrik Horve <karlhenh@stud.ntnu.no>
2023-08-21 14:43:14 +0200

    .

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # IIKG3005_InfrastructureAsCode
 Project repo for the Infrastructure As Code class at NTNU
+
+## How-to:
+1. Rename 'temp_dotEnv.txt' to '.env'
+2. Make sure your '.gitignore' folder has the line '.env'
+3. Fill out the '.env' file.
+4. Run one of the connection scripts below
+
+## Connecting to Azure
+With bash:
+```. /path/to/connect_az_cli.sh```
+
+With powershell core:
+```. /path/to/connect-Azure.ps1```
--- connect-Azure.ps1
+++ connect-Azure.ps1
@@ -1,8 +1,17 @@
+# Importing Azure module. Installs the module if it's not installed
+$installedAzModules = Get-InstalledModule | Where-Object {$_.Name -like "Az.*"}
+if (!$installedAzModules) {
+    Install-Module -Name Az -Repository PSGallery -Force 
+}
+Import-Module -Name Az
+
+# Load envVars
 $dotEnv = Join-Path -Path $PSScriptRoot -ChildPath ".env"
 Get-Content -Path $dotEnv | ForEach-Object {
     $key, $value = $_.Split('=')
     Set-Item -Path "env:$key" -Value $value
 }
+# Create credentialSet and establish a connection to Azure
 $securePassword = ConvertTo-SecureString -string $env:appSecret -AsPlainText -Force
 $Credential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $env:appId, $securePassword
 Connect-AzAccount -ServicePrincipal -TenantId $env:tenantId -Credential $Credential
